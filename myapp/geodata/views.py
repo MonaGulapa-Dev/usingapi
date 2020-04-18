01488c54bab413e0f909044a656a604ed82712f0
MonaGulapa-Dev <55570230+MonaGulapa-Dev@users.noreply.github.com>
2020-04-18 23:06:34 +0800

    Update views.py

--- myapp/geodata/views.py
+++ myapp/geodata/views.py
@@ -1,46 +1,42 @@
 from django.shortcuts import render
 from django.conf import settings
-import requests, json
-
+import json
+import requests
+from urllib.request import urlopen
+from datetime import datetime
 
 
 def home(request):
-    is_cached = ('geodata' in request.session)
-    auth=settings.IPFIND_API
-    google_api=settings.GOOGLE_API
-    iploc = 'https://api.ipfind.com/me?auth=' + auth
-    if not is_cached:
-        response = requests.get(iploc)
-        request.session['geodata'] = response.json()
-        # print(type(geodata))  #dict
-        # for key in geodata.keys():
-        #     value = geodata[key]
-        #     print(key, '=', value)
+    ip_address = request.META.get('HTTP_X_FORWARDED_FOR', '')
+    url = 'https://api.ipdata.co/' + ip_address + '?api-key=' + settings.IP_STACK_KEY
+    response = requests.get(url)
+    response2 = urlopen(url)
+    json_data = response2.read().decode("utf-8")
+    geodata = json.loads(json_data)
+    language = geodata['languages']
+    language = language[0]
+    language = language['name']
+    currency = geodata['currency']
+    currency = 'Name: ' + currency['name'] + '  ' + 'Code: ' + \
+        currency['code'] + '  ' + 'Symbol: ' + currency['symbol']
+    time_zone = geodata['time_zone']
+    timezone = time_zone['name'] + ' ' + \
+        time_zone['abbr'] + ' ' + time_zone['offset']
+    today_date = time_zone['current_time']
+    today_date1 = today_date[:10]
+    date_object = datetime.strptime(today_date1, '%Y-%m-%d').date()
+    today_is = datetime.strftime(date_object, "%B %d, %Y")
+    return render(request, 'geodata/home.html', {
+        'ip': geodata['ip'],
+        'country': geodata['country_name'],
+        'continent': geodata['continent_name'],
+        'language': language,
+        'currency': currency,
+        'timezone': timezone,
+        'today_is': today_is,
 
-        # # data = json.load(geodata)
-        # context = {'geodata1': geodata}
-        # print(type(context))
-        # for key in context.keys():
-        #     value = context[key]
-        #     print(key, '=', value)
-
-    geodata = request.session['geodata']
-
-    return render(
-        request,
-        'geodata/home.html',
-        {
-            'ip': geodata['ip_address'],
-            'country': geodata['country'],
-            'city': geodata['city'],
-            'region': geodata['region'],
-            'timezone': geodata['timezone'],
-            'currency': geodata['currency'],
-            'languages': geodata['languages'],
-            'latitude': geodata['latitude'],
-            'longitude': geodata['longitude'],
-            'api_key': google_api,
-            'is_cached': is_cached
-        },
-    )
- 
+        # 'location': geodata['location'],
+        'latitude': geodata['latitude'],
+        'longitude': geodata['longitude'],
+        'api_key': settings.GOOGLE_API
+    })
